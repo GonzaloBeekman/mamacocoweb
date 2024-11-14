44ebe21e5d947b869dc32d3b206f66f0ce5fd2da
gonzalo <gonzalobeekman2@gmail.co>
2024-11-14 12:52:32 -0300

    14/11

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,10 @@ import './Style.css';
 const Header = () => {
   return (
     <header>
-      
-      <div className="logo">
-        <a href="/"><img src="./img/remove.png" alt="Logo" /></a>
-      </div>
       <nav className="navbar">
         <ul>
           <li className="boton"><a href="/">Menu</a></li>
-          <li className="boton"><a href="/login">Inicio Sesion</a></li>
+          <li className="boton"><a href="/login">Iniciar Sesion</a></li>
           <li className="boton"><a href="/manual">Ayuda</a></li>
         </ul>
       </nav>
